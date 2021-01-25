7b01c0e1c1d88c79fce6e168f5ed40bf9f009d53
iampkt <iampkt22@gmail.com>
2021-01-25 23:09:35 +0900

    입력받은 정수 더하기(a+b)

---
+++ 1-5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace _1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = Console.ReadLine();
+
+            //int a = 1;
+            //int b = 2;
+
+            //Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            int a = int.Parse(ss[0]);
+            int b = int.Parse(ss[1]);
+            Console.WriteLine(a + b);
+
+        }
+    }
+}
