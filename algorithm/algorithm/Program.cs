98cbdb51f10a6fe52a696df7a72b42bcc3b60d27
iampkt <iampkt22@gmail.com>
2021-01-31 16:50:28 +0900

    a 더하기 b

---
+++ algorithm/algorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using algorithm.level_1;
+
+namespace algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            AaddB aaddB = new AaddB(s);
+            aaddB.GetResult();
+        }
+    }
+}
---
+++ algorithm/algorithm/level_1/AaddB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace algorithm.level_1
+{
+    public class AaddB
+    {
+        int a = 0;
+        int b = 0;
+
+        public AaddB(string s)
+        {
+            string[] ss = s.Split();
+            a = int.Parse(ss[0]);
+            b = int.Parse(ss[1]);
+        }
+
+        public void GetResult()
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
